a263c9fe51bf5c7da9f1b8bbe55f0a0140ca2336
CAIO FERNANDES BARBOSA <25203662@CFP127ALUNO.LOCAL>
2025-11-03 16:56:45 -0300

    usando vetor e loop para a realização de uma tabuada

---
+++ src/br/senai/sp/jandira/tabuada/Tabuada.java
@@ -0,0 +1,11 @@
+package br.senai.sp.jandira.tabuada;
+
+import br.senai.sp.jandira.tabuada.model.TabuadaApp;
+
+public class Tabuada {
+    public static void main(String[] args) {
+
+        TabuadaApp tabuada = new TabuadaApp();
+        tabuada.receberDados();
+    }
+}
---
+++ src/br/senai/sp/jandira/tabuada/model/TabuadaApp.java
@@ -0,0 +1,60 @@
+package br.senai.sp.jandira.tabuada.model;
+
+import java.util.Scanner;
+
+public class TabuadaApp {
+
+    public int multiplicando;
+    public int multiplicadorInicial;
+    public int multiplicadorFinal;
+    public String[]tabuada;
+
+
+    public void receberDados(){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Digite o multiplicando: ");
+        multiplicando = leitor.nextInt();
+
+        System.out.print("Digite o multiplicador Inicial: ");
+        multiplicadorInicial = leitor.nextInt();
+
+        System.out.print("Digite o multiplicador final: ");
+        multiplicadorFinal = leitor.nextInt();
+
+        calcularDados();
+    }
+
+    public void calcularDados(){
+        int apoio = 0;
+
+        if (multiplicadorFinal < multiplicadorInicial){
+            apoio = multiplicadorFinal;
+            multiplicadorFinal = multiplicadorInicial;
+            multiplicadorInicial = apoio;
+        }
+
+        int tamanho = multiplicadorFinal - multiplicadorInicial + 1;
+        tabuada = new String[tamanho];
+
+        int i = 0;
+        while (i < tabuada.length){
+            int produto = multiplicando * multiplicadorInicial;
+            tabuada[i] = multiplicando + " x " + multiplicadorInicial + " = " + produto;
+            multiplicadorInicial = multiplicadorInicial + 1;
+            i = i + 1;
+        }
+
+        exibirTabuada();
+    }
+    public void exibirTabuada(){
+        System.out.println("Resultado da sua tabuada");
+
+        int i = 0;
+        while (i < tabuada.length){
+            System.out.println(tabuada[i]);
+            i++; // Mesma coisa que i = i + 1
+
+        }
+    }
+}
